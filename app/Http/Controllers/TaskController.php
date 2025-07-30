bb5f166e5463819f946e8449f581a6a9aab41111
William Daniel <danielw023@protonmail.com>
2025-07-30 09:02:21 +0100

    Created task controller as well as store task request for form validation.

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreTaskRequest;
+use App\Models\Task;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\View\View;
+
+class TaskController extends Controller
+{
+    public function index(): View
+    {
+        $tasks = Task::all(['id', 'description', 'completed']);
+
+        return view('tasks', compact('tasks'));
+    }
+
+    public function store(StoreTaskRequest $request): RedirectResponse
+    {
+        $validated = $request->validated();
+
+        Task::create($validated);
+
+        return to_route('tasks.index')->with('success', 'Task created successfully');
+    }
+
+    public function update(Task $task): RedirectResponse
+    {
+        $task->update([
+            'completed' => true
+        ]);
+
+        return to_route('tasks.index')->with('success', 'Task updated successfully!');
+    }
+
+    public function destroy(Task $task): RedirectResponse
+    {
+        $task->delete();
+
+        return to_route('tasks.index')->with('success', 'Task deleted successfully!');
+    }
+}
---
+++ app/Http/Requests/StoreTaskRequest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreTaskRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'description' => 'required|string|max:255|min:3',
+        ];
+    }
+}
