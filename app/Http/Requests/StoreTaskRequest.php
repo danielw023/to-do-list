78fa4b8895e68b1bd9304f707f77b692b0c107d4
William Daniel <danielw023@protonmail.com>
2025-07-30 10:56:15 +0100

    Removed validation rule minimum of 3 for description in store task request as requirement does not specify this.

--- app/Http/Requests/StoreTaskRequest.php
+++ app/Http/Requests/StoreTaskRequest.php
@@ -14,7 +14,7 @@ class StoreTaskRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'description' => 'required|string|max:255|min:3',
+            'description' => 'required|string|max:255',
         ];
     }
 }
