f7461d54e95d6ac931142fa3a187cc3a58ddd731
William Daniel <danielw023@protonmail.com>
2025-07-30 08:52:28 +0100

    Created component and view for head, header and footer

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,3 @@
+<footer class="text-center py-3 mt-5">
+    <p class="mb-0">&copy; 2020 All Rights Reserved</p>
+</footer>
---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,3 @@
+<header class="d-flex justify-content-start align-items-center py-3" role="banner">
+    <img src="{{ asset('images/logo.png') }}" alt="MLP To-Do Company Logo" class="img-fluid">
+</header>
